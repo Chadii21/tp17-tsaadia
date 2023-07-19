92bcb27fe6fcd993b93cb6a5cec1282c9cc9399a
chadii-t <sadiaatraore30@gmail.com>
2023-07-19 17:01:11 +0000

    tp17_tsaadia

---
+++ index.js
@@ -0,0 +1,11 @@
+// SELECTEURS
+// document.querySelector("h4").style.background = "yellow";
+//  const baliseHTML = document.querySelector("h4");
+
+// Click event
+const questionContainer = document.querySelector(".click-event")
+;
+
+questionContainer.addEventListener("click", () => {
+    questionContainer.getElementsByClassName.background="red"
+});
